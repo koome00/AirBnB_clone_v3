143a831b5e169f620cd0c0de44d3505b8185e50f
koome00 <koomemc@gmail.com>
2024-01-08 13:24:58 -0800

    api blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Module containing views """
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status")
 def status():
-    """ returns a JSON """
-    status = {"status": "OK"}
-    return jsonify(status)
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
