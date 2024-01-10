f19bf31c3cd691d4698515f6ca86479609a62c18
koome <koomemc@gmail.com>
2024-01-10 17:12:36 +0300

    get

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module containing Review View """
 from api.v1.views import app_views
-from models.place import Place, place_amenity
+from models.place import Place, 
 from models.amenity import Amenity
 from flask import Flask, jsonify, abort, request
 from models import storage
@@ -22,20 +22,3 @@ def get_amenity(place_id):
             ams.append(k.to_dict())
         return jsonify(ams)
 
-
-@app_views.route("/places/<place_id>/amenities/<amenity_id>",
-                 methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
-    """
-    Delete amenity in place given place and amenity id
-    """
-    place = storage.all(Place, place_id)
-    amenity = storage.all(Amenity, amenity_id)
-
-    if place is None or amenity is None:
-        abort(404)
-    if amenity not in place.amenities:
-        abort(404)
-    place.amenities.remove(amenity)
-    place.save()
-    return jsonify({}), 200
